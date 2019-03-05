b6652374b039d4cb91367d9a9e0de5b8cd0a837e
Denis Tuskenis <dtuskenis@qulix.com>
2019-03-05 13:55:49 +0300

    implement simple linear gradient view with setable components

--- app/src/main/java/com/dtuskenis/gradients/sample/AppActivity.kt
+++ app/src/main/java/com/dtuskenis/gradients/sample/AppActivity.kt
@@ -1,7 +1,10 @@
 package com.dtuskenis.gradients.sample
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.dtuskenis.gradients.LinearGradientView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class AppActivity : AppCompatActivity() {
 
@@ -9,5 +12,13 @@ class AppActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val gradientColorComponents =
+            listOf(Color.RED to 0.0f,
+                   Color.GREEN to 0.5f,
+                   Color.BLUE to 1.0f)
+                .map { LinearGradientView.ColorComponent(it.first, it.second) }
+
+        gradientView.setColorComponents(gradientColorComponents)
     }
 }
