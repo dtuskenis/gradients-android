b48c91a01aac65ad3e89c5d6ab0cb01c83126171
Denis Tuskenis <dtuskenis@qulix.com>
2019-03-05 10:51:46 +0300

    add template for sample app

---
+++ app/src/main/java/com/dtuskenis/gradients/sample/AppActivity.kt
@@ -0,0 +1,13 @@
+package com.dtuskenis.gradients.sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
